e81bdcf8d94ea3ec1eabd80ec14e3bdddd1663e0
proj <lvhaost2004@gmail.com>
2024-12-15 16:15:09 +0700

    Updates

---
+++ precache-manifest.29344048eb60eb64db3d03eed53f4f33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c0e89294a7b7ed275d8d8b3cf44536b",
+    "url": "/proj/index.html"
+  },
+  {
+    "revision": "df61643485929226b18f",
+    "url": "/proj/static/css/main.15bdd851.chunk.css"
+  },
+  {
+    "revision": "e14469b42b5b383e5113",
+    "url": "/proj/static/js/2.23a3f531.chunk.js"
+  },
+  {
+    "revision": "00a0c8f113ef139e5b17d28db2455a34",
+    "url": "/proj/static/js/2.23a3f531.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df61643485929226b18f",
+    "url": "/proj/static/js/main.d61f6cef.chunk.js"
+  },
+  {
+    "revision": "047557c5ccde28c8b36c",
+    "url": "/proj/static/js/runtime-main.ec9a19c4.js"
+  }
+]);
